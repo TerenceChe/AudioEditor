e89d4808cb201ed0df59141c257b728a3940671e
tnc1 <tnc1@sfu.ca>
2022-05-14 14:09:50 -0700

    initial commit with react frontend django backend

---
+++ frontend/src/components/App.js
@@ -0,0 +1,16 @@
+import "../styles/App.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+
+function App() {
+  return (
+    <Container>
+      <Row>
+        <Button variant="primary">Primary</Button>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
