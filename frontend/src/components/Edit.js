1f8e98486652b54918591e68c63b40603382b8a2
tnc1 <tnc1@sfu.ca>
2022-06-14 14:32:36 -0700

    clickable text

--- frontend/src/components/Edit.js
+++ frontend/src/components/Edit.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
@@ -6,27 +8,63 @@ import { useSelector } from "react-redux";
 
 import "../styles/Edit.css";
 
+//https://letsbuildui.dev/articles/building-an-audio-player-with-react-hooks
 function Edit() {
   const data = useSelector((state) => state);
+  const newArr = new Array(Object.keys(data.audioData).length).fill(false);
+  const [isActive, setActive] = useState(newArr);
+
+  const [audio, setAudio] = useState(null);
+  const [trackIndex, setTrackIndex] = useState(0);
+  const [trackProgress, setTrackProgress] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  useEffect(() => {
+    var audio = new Audio(URL.createObjectURL(data.file));
+    setAudio(audio);
+    console.log(audio);
+  }, [data.file]);
 
   const renderObject = (data) => {
     return Object.entries(data).map(([key, value], i) => {
       return (
-        <div key={key}>
-          {value[0]}: {value[1]} - {value[2]}
+        <div
+          className="selected"
+          data-id={key}
+          key={key}
+          onClick={handleTextClick}
+        >
+          {value[0]}
         </div>
       );
     });
+  };
+
+  const handlePlay = (event) => {
+    audio.play();
+  };
+
+  const handlePause = (event) => {
+    audio.pause();
+  };
+
+  const handleTextClick = (event) => {
+    const index = event.target.dataset.id;
+    var tmpArr = isActive;
+    isActive[index] = !isActive[index];
+    setActive(tmpArr);
+    event.target.classList.toggle("selected");
   };
 
   return (
     <Container>
       <Row>
         <p>{data.file.name}</p>
+        <Button onClick={handlePlay}>Play</Button>
+        <Button onClick={handlePause}>Pause</Button>
       </Row>
-      <Row>
-        <p>{renderObject(data.audioData)}</p>
-      </Row>
+      <br />
+      <div>{renderObject(data.audioData)}</div>
     </Container>
   );
 }
